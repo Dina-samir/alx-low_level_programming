912c70f886f13f3014d22c93031fe77c7bc9ff49
Dina Samir <dina24samir@gmail.com>
2023-09-22 23:45:25 -0700

    d

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- * 0-reset_to_98 : takes a pointer to an int as parameter and updates the value it points to to 98
- *
+ * reset_to_98 -  takes a pointer to an int as parameter and updates the value it points to to 98
  * @n: int pointer  parameter
  *
- * return :Nothing
+ * return: Nothing
  */
 
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 }	
-	*n=98;
+	*n = 98;
 }	
 	
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
-* swap_int: function that swaps the values of two integers
-* *a :frist integer parameter
-* *b :second integer parameter
-* Return : Nothing
+* swap_int - function that swaps the values of two integers
+*
+* @a: frist integer parameter
+* @b: second integer parameter
+*
+* Return: void
 */
 
 void swap_int(int *a, int *b)
 {
     int temp = *a;
-    *a= *b;
+    *a = *b;
     *b = temp;
 }
