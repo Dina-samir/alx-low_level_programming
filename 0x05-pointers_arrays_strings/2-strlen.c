fa270fa3a47cc80fea4ebb438fb646f8ba0e12af
Dina Samir <dina24samir@gmail.com>
2023-09-22 23:09:33 -0700

    ‘a’

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strlen: function that returns the length of a string
+* *s :string
+* Return : length of a string
+*/
+
+int _strlen(char *s):
+{
+    int counter=0;
+    while *s != "\0"
+    {
+        ++counter;
+        ++s;
+        printf(*s)
+        printf(s)
+
+    }
+    
+}
+
