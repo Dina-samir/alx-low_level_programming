fba34cc7a35bdda2409a3c93f12921f672777b50
Dina Samir <dina24samir@gmail.com>
2023-09-22 22:03:46 -0700

    ‘a’

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+* swap_int: function that swaps the values of two integers
+* *a :frist integer parameter
+* *b :second integer parameter
+* Return : Nothing
+*/
+
+void swap_int(int *a, int *b):
+{
+    int temp = *a;
+    *a= *b;
+    *b = temp;
+}
