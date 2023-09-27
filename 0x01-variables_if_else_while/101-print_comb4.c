9a0d7851e4d0f07b6f0b4353ff99131474e92665
Dina-samir <dina24samir@gmail.com>
2023-09-27 10:55:03 +0300

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -24,7 +24,9 @@ int main(void)
 					putchar(i);
 					putchar(g);
 					if (i == 56 && e == 55 && g == 57)
-					{break;}
+					{
+						break;
+					}
 					putchar(',');
 					putchar(' ');
 				}
