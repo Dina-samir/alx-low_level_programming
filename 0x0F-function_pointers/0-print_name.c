7bc9133aec71c1e0899d230af6b80b2c79aff9a3
Dina-samir <dina24samir@gmail.com>
2023-10-12 23:11:54 +0300

    .

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,11 @@
 #include "function_pointers.h"
-
 /**
  * print_name - print the name
  * @name: name to print
  * @f: function to use
- *
- * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name && f)
-        f(name);
+	if (name && f)
+		f(name);
 }
