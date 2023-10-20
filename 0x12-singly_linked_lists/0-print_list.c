93304b1973795fd699094b25175775154277f421
Dina Samir <dina24samir@gmail.com>
2023-10-19 17:50:28 -0700

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,24 +6,23 @@
  * @h: The head of the linked list
  * Return: The number of nodes of the linked list
  */
+
 size_t print_list(const list_t *h)
 {
-    unsigned int node_count = 0;   
+	unsigned int i = 0;
 
-    while (h)   
-    {
-        if (h->str)
-        {
-            printf("[%u] %s\n", h->len, h->str);
-        }
-        else   
-        {
-            printf("[0] (nil)\n");
-        }
-        
-        node_count++;   
-        h = h->next;    
-    }
-
-    return (node_count);
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		i++;
+		h = h->next;
+	}
+	return (i);
 }
