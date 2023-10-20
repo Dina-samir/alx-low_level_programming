8191ae9d07a2d41dd8088b4e7b08314dffb03383
Dina Samir <dina24samir@gmail.com>
2023-10-20 03:08:44 -0700

    .

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,14 +5,16 @@
  * @head: points to a node on the list
  * Return: void
  */
-void free_list(list_t *head)
-{
-	list_t *temp;
 
-	while  ((temp = head) != NULL)
-	{		
-		head = head->next;
-		free(temp->str);
-		free(temp);
-	}
+void free_list(list_t *head) {
+    list_t *current = head;
+    list_t *next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current->str);
+        free(current);
+        current = next;
+    }
 }
+
