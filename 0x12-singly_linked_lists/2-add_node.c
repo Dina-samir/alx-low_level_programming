edca48f4eca2778a5ea5d259c28a0b133fe166f9
Dina Samir <dina24samir@gmail.com>
2023-10-20 02:39:27 -0700

    .

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,44 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
-/**
- * get_length - returns the length of a string
- * @str: string to determine the length of
- *
- * Return: length of @str
- */
-unsigned int get_length(const char *str)
-{
-	unsigned int i, length = 0;
+list_t *add_node(list_t **head, const char *str) {
+    list_t *new_node;
 
-	if (str == NULL)
-		return (0);
+    if (str == NULL) {
+        return NULL; 
+    }
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		length++;
-		i++;
-	}
-	return (length);
+    new_node = malloc(sizeof(list_t));
+    if (new_node == NULL) {
+        return NULL; 
+    }
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL) {
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->next = *head;
+    *head = new_node;
+
+    return new_node;
 }
 
-/**
- * add_node - add a node at the beginning
- * @head: pointer to first node
- * @str: string
- * Return: pointer to list
- */
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new = (list_t *) malloc(sizeof(list_t));
-	unsigned int len;
 
-	if (new == NULL)
-		return (NULL);
-	len = get_length(str);
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head) = new;
-	return (*head);
-}
